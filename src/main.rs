e9d2edad179b3ed5a6ec5b6a76d95cc1019619b7
井澤伊織 <izawa.iori.tan@gmail.com>
2022-04-20 17:32:17 -0400

    start working with amethyst

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use amethyst::{
+    ecs::{World, WorldExt},
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
 mod utils;
 
-fn main() {
+fn main() -> amethyst::Result<()> {
     // dostuff
+    Ok(())
 }
