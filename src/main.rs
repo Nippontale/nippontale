c02e393dd3035595a0cc0b4d4dc949c26bb64b88
キャンド <amehikoji@gmail.com>
2022-04-21 22:43:14 -0400

    migrate to bevy

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use amethyst::{
-    ecs::{World, WorldExt},
-    prelude::*,
-    renderer::{
-        plugins::{RenderFlat2D, RenderToWindow},
-        types::DefaultBackend,
-        RenderingBundle,
-    },
-    utils::application_root_dir,
-};
+#![allow(clippy::all)]
+#![allow(warnings)]
 
-mod utils;
+use bevy::prelude::*;
 
-fn main() -> amethyst::Result<()> {
-    amethyst::start_logger(Default::default());
-    let app_root = application_root_dir()?;
-    let display_config_path = app_root.join("config").join("display.ron");
-    Ok(())
+pub mod utils;
+pub mod imp;
+pub mod base;
+
+/// main function
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(utils::setup)
+        .run();
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,12 @@
+use bevy::prelude::*;
+
 mod battle;
 mod world;
 
 mod dialogue;
 mod graphics;
+
+/// game setup
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
