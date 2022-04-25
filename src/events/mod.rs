020972bd12fed5a6a89bf3e3f664eb72226919cd
キャンド <amehikoji@gmail.com>
2022-04-24 23:53:53 -0400

    prevent text from reappearing when reopening textbox

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -4,19 +4,22 @@ use crate::prelude::*;
 #[derive(Component)]
 pub struct Savepoint;
 
-pub fn player_use_input(keys: Res<Input<KeyCode>>, mut q: Query<(&PlayerControlled, &mut Touching)>, mut q2: Query<(&mut Textbox, &mut Visibility)>, mut logger: ResMut<Logger>, mut ntt: ResMut<NewTextboxText>) {
+pub fn player_use_input(keys: Res<Input<KeyCode>>, mut q: Query<(&PlayerControlled, &mut Touching)>, mut q2: Query<(&mut Textbox, &mut Visibility, Option<&mut Text>)>, mut logger: ResMut<Logger>, mut ntt: ResMut<NewTextboxText>) {
     for (ply, mut tch) in q.iter_mut() {
         if ply.controlled && tch.savepoint && keys.just_pressed(KeyCode::E) && !tch.in_scene {
             tch.savepoint = false;
             tch.in_scene = true;
-            ntt.new_text("You are filled with pride and honor!", 30.);
+            ntt.new_text("You are filled with pride and honor!", 40.);
             ntt.text = String::from("");
-            for (mut txb, mut vis) in q2.iter_mut() {
+            for (mut txb, mut vis, mut txt) in q2.iter_mut() {
                 txb.active = true;
                 vis.is_visible = txb.active;
+                if let Some(mut tx) = txt {
+                    tx.sections[0].value = String::from("");
+                } 
             }
         } else if keys.just_pressed(KeyCode::E) && ntt.complete == ntt.text && tch.in_scene {
-            for (mut txb, mut vis) in q2.iter_mut() {
+            for (mut txb, mut vis, txt) in q2.iter_mut() {
                 txb.active = false;
                 vis.is_visible = txb.active;
             }
