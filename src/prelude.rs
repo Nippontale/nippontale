22f079d2ad08a35ff18f9f78812c237f377d64da
井澤伊織 <izawa.iori.tan@gmail.com>
2022-05-16 10:56:12 -0400

    move function

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,7 +4,7 @@ pub use crate::Deletor;
 pub use crate::graphics;
 pub use crate::events;
 pub use crate::utils::scenes::SceneUpdater;
-pub use crate::utils::scenes::{scene_00::spawn_scene_00};
+pub use crate::utils::scenes::{scene_00::spawn_scene_00, spawn_savepoint};
 pub use crate::physics::collisions::{HitboxBundle, HitboxSize, Map};
 pub use crate::physics::{Moving, MainCharacter, SyncHitboxSize, Touching, PlayerControlled};
 pub use crate::dialogue::{Textbox, NewTextboxText, sync_textbox_text, txb_tick, sync_textbox_vis};
--- src/utils/scenes/mod.rs
+++ src/utils/scenes/mod.rs
@@ -1,3 +1,6 @@
+use bevy::prelude::*;
+use crate::prelude::*;
+
 pub mod scene_00;
 
 pub struct SceneUpdater {
@@ -9,4 +12,28 @@ impl Default for SceneUpdater {
     fn default() -> Self {
         SceneUpdater { num:  0, b: true }
     }
+}
+
+pub fn spawn_savepoint(mut commands: &mut Commands, x: f32, y: f32, tat: Handle<TextureAtlas>) {
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: tat,
+            transform: Transform::from_xyz(x, y, 0.),
+            sprite: TextureAtlasSprite { custom_size: Some(Vec2::new(96., 96.)), ..Default::default()},
+            ..default()
+        })
+        // deleted as part of the map
+        .insert(Map {})
+        // HitboxBundle to take care of player - entity collisions
+        // this will auto sync with the texture atlas sprite's size
+        // so we simply use default.
+        .insert(HitboxSize { size: Size { width: 86., height: 86.} })
+        // save point event marker, marks this entity 
+        // as a save point so it can be used as so
+        // by the player.
+        .insert(events::Savepoint {})
+        // animated bundle to animate the spritesheet 
+        // changes sprite every (duration)s 
+        // and repeats if (repeating) is set to true
+        .insert_bundle(graphics::AnimatedBundle::from_seconds(0.3, true));
 }
