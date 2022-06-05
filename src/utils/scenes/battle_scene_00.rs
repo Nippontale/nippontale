ddf0074b4d80b04f66f74a7b06768ce638de7ea1
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-06-04 21:07:07 -0400

    trying to change background color

--- src/utils/scenes/battle_scene_00.rs
+++ src/utils/scenes/battle_scene_00.rs
@@ -1,9 +1,11 @@
 use bevy::prelude::*;
 use crate::prelude::*;
+use bevy::render::color::*;
 
 pub fn spawn_battle_scene_00(mut commands: Commands, 
     mut scene_updater: ResMut<SceneUpdater>, 
     deletor: Res<Deletor>,
+    mut bg_color: ResMut<ClearColor>,
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
     mut q: Query<(&mut Visibility), With<PlayerControlled>>,
@@ -11,6 +13,8 @@ pub fn spawn_battle_scene_00(mut commands: Commands,
 ) {
     if scene_updater.b && scene_updater.num == 256 && !deletor.b {
         scene_updater.b = false;
+        println!("{:?}", bg_color);
+        bg_color = (Color::hsla(252., 0.33, 0.18, 0.0));
         // for (mut v) in q.iter_mut() {
         //     v.is_visible = false;
         // }
